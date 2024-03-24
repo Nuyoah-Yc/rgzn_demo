027fd03d3dfa8a2aec3acbd2c64a4962abd66eca
Nuyoah <2403930241@qq.com>
2024-03-24 11:04:17 +0800

    更新

--- 第一阶段/demo1/demo1.py
+++ 第一阶段/demo1/demo1.py
@@ -38,12 +38,13 @@ def detector_face_image(prototxt_path,model_path,image_path):
     cv2.imshow("Output_Image",image)
     cv2.waitKey(0)
 
+
 if __name__=="__main__":
     # prototxt文件路径
-    prototxt_path = "./face_detector/deploy.prototxt"
+    prototxt_path = "model/deploy.prototxt"
     # model文件路径
-    model_path =  "./face_detector/res10_300x300_ssd_iter_140000_fp16 (1).caffemodel"
+    model_path = "model/res10_300x300_ssd_iter_140000_fp16 (1).caffemodel"
     # image路径
-    image_path =  "./face.jpg"
+    image_path =  "./demo1.png"
 
     detector_face_image(prototxt_path, model_path, image_path)

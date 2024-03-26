3b355daa2f890c8ebc4f02db40b7939c03af1e6c
29448 <2944891413@qq.com>
2024-03-26 12:08:04 +0800

    添加

--- 第二阶段/本地数据集的制作.py
+++ 第二阶段/本地数据集的制作.py
@@ -7,18 +7,18 @@ import os
 
 
 class Mask_dataset(Dataset):
-    def __init__(self, csv_file, root_dir, transform=None, ):
-        self.landmarks_frame = pd.read_csv(csv_file)
-        self.root_dir = root_dir
-        self.transform = transform
-
+    def __init__(self,csv_file, root_dir, transform=None,):
+        self.landmarks_frame=pd.read_csv(csv_file)
+        self.root_dir=root_dir
+        self.transform=transform
     def __len__(self):
         return len(self.landmarks_frame)
-
-    def __getitem__(self, idx):
-        img_path = os.path.join(self.root_dir, self.landmarks_frame.iloc[idx, 0])
-        label = self.landmarks_frame.iloc[idx, 1]
-        img = Image.open(img_path).convert("RGB")
+    def __getitem__(self,idx):
+        img_path= os.path.join(self.root_dir,self.landmarks_frame.iloc[idx,0])
+        label = self.landmarks_frame.iloc[idx,1:]
+        label = np.array([label],dtype=float).reshape(-1,2)
+        img=Image.open(img_path).convert("RGB")
+        sample ={"img":img,"label":label}
         if self.transform is not None:
-            img = self.transform(img)
-        return img, label
+            smaple= self.transform(sample)
+        return smaple

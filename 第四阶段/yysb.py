a4532c737aed02be68e97ce4e2bae6bfa0d931cf
Nuyoah <2403930241@qq.com>
2024-03-26 14:00:56 +0800

    更新

---
+++ 第四阶段/yysb.py
@@ -0,0 +1,19 @@
+import speech_recognition as sr
+
+# 初始化识别器
+r = sr.Recognizer()
+
+# 从麦克风捕获音频
+with sr.Microphone() as source:
+    print("请说些什么：")
+    audio = r.listen(source)
+
+# 尝试识别音频中的语音
+try:
+    # 使用Google网络语音API来识别音频
+    text = r.recognize_google(audio, language="zh-CN")  # 设置语言为中文
+    print("你说了：" + text)
+except sr.UnknownValueError:
+    print("Google网络语音API无法理解音频")
+except sr.RequestError as e:
+    print(f"无法从Google网络语音API请求结果；{e}")
